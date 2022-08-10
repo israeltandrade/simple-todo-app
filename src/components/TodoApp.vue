14cf0070bb60ef5b9418fe2468a7145216473aaf
Israel Andrade DEV <israeltandrade@hotmail.com>
2022-08-10 15:34:15 -0300

    basic structure implemented

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -9,7 +9,32 @@
     </div>
     
     <!-- Task table -->
-    
+    <table class="table table-bordered mt-5">
+  <thead>
+    <tr>
+      <th scope="col">Task</th>
+      <th scope="col">Status</th>
+      <th scope="col" class="text-center">#</th>
+      <th scope="col" class="text-center">#</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th>Criar aplicativo Vue.js</th>
+      <td>Doing</td>
+      <td>
+        <div class="text-center">
+          <span class="fa fa-pen"></span>
+        </div>
+      </td>
+      <td>
+        <div class="text-center">
+          <span class="fa fa-trash"></span>
+        </div>
+      </td>
+    </tr>
+  </tbody>
+</table>
 </div>
 </template>
 
