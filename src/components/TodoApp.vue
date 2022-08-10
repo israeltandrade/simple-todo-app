aae8358d48b61c11ad809ebb9294b0754325bcfa
Israel Andrade DEV <israeltandrade@hotmail.com>
2022-08-10 15:16:00 -0300

    initial files and configuration

---
+++ src/components/TodoApp.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <h2 class="text-center mt-5">Todo App</h2>
+
+    <!-- Input -->
+    <div class="d-flex">
+      <input type="text" placeholder="Enter task" class="form-control">
+      <button class="btn btn-warning rounded-0">SUBMIT</button>
+    </div>
+    
+    <!-- Task table -->
+    
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
