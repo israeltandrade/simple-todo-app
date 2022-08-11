8c6fd1ed6fa8f3eb7389bdbcacc83404d1924b29
Israel Andrade DEV <israeltandrade@hotmail.com>
2022-08-11 18:01:23 -0300

    implementation of status change

--- src/components/TodoApp.vue
+++ src/components/TodoApp.vue
@@ -20,8 +20,17 @@
   </thead>
   <tbody>
     <tr v-for="(task, index) in tasks" :key="index">
-      <td>{{ task.name }}</td>
-      <td>{{ task.status }}</td>
+      <td>
+        <span :class="{'done': task.status === 'Done'}">{{ task.name }}</span>
+      </td>
+      <td style="width: 120px">
+        <span @click="changeStatus(index)" class="pointer"
+        :class="{'text-danger': task.status === 'To-Do',
+                 'text-warning': task.status === 'Doing',
+                 'text-success': task.status === 'Done',}">
+          {{ task.status }}
+        </span>
+      </td>
       <td>
         <div class="text-center" @click="editTask(index)">
           <span class="fa fa-pen"></span>
@@ -52,13 +61,14 @@ export default {
       tasks: [
         {
           name: 'Create Vue.js App',
-          status: 'Done'
+          status: 'To-Do'
         },
         {
           name: 'Make it awesome!',
-          status: 'Doing'
+          status: 'To-Do'
         }
       ],
+      statuses: ['To-Do', 'Doing', 'Done']
     }
   },
 
@@ -84,7 +94,23 @@ export default {
     editTask(index) {
       this.task = this.tasks[index].name;
       this.editedTask = index;
+    },
+
+    changeStatus(index) {
+    let newIndex = this.statuses.indexOf(this.tasks[index].status);
+    if (++newIndex > 2) newIndex = 0;
+    this.tasks[index].status = this.statuses[newIndex];
     }
   }
 }
 </script>
+
+<style scoped>
+.pointer {
+  cursor: pointer;
+}
+
+.done {
+  text-decoration: line-through;
+}
+</style>
